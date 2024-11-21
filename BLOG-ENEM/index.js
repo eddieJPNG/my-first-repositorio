8d4cb6f5f92ed1820f5ca4fa0a5b40b6294dc588
eddie <rochaedsonn12@gmail.com>
2024-11-21 14:59:37 -0300

    upp 21//2222

--- BLOG-ENEM/index.js
+++ BLOG-ENEM/index.js
@@ -1,55 +1,20 @@
-document.getElementById("#imagem").onclick = function() { var minhaImagem = document.getElementById("#imagem"); minhaImagem.classList.toggle("active");}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function addComment() {
+  var nameInput = document.getElementById("nameInput");
+  var commentInput = document.getElementById("commentInput");
+  var commentsDiv = document.getElementById("comments");
+  var name = nameInput.value;
+  var commentText = commentInput.value;
+  if (name.trim() !== "" && commentText.trim() !== "") {
+    var newComment = document.createElement("div");
+    newComment.classList.add("comment");
+    var namePara = document.createElement("p");
+    namePara.innerHTML = "<strong>" + name + "</strong>";
+    newComment.appendChild(namePara);
+    var commentPara = document.createElement("p");
+    commentPara.textContent = commentText;
+    newComment.appendChild(commentPara);
+    commentsDiv.appendChild(newComment);
+    nameInput.value = "";
+    commentInput.value = "";
+  }
+}
